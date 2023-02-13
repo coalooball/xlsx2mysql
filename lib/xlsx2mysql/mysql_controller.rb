68eda989eb7a5cd2b471bc536ecdab2a6bbfbfa9
Cyan <cyan_cg@qq.com>
2023-02-13 16:11:57 +0800

    Add MysqlController#initialize

--- lib/xlsx2mysql/mysql_controller.rb
+++ lib/xlsx2mysql/mysql_controller.rb
@@ -1,5 +1,8 @@
 class MysqlController
   attr_accessor :host, :port, :user, :password, :database, :charset
+  def initialize
+    @my = nil
+  end
 
   def connect(db_info_path, env)
     mysql_url = "mysql://#{user}:#{password}@#{host}:#{port}/#{database}?charset=#{charset}"
@@ -7,10 +10,12 @@ class MysqlController
   end
 
   def describe_table(table)
+    raise "MySQL is not connected!" unless @my
     @my.query("DESCRIBE #{table}").entries
   end
-
+  
   def insert_one_record(table, key_values)
+    raise "MySQL is not connected!" unless @my
     keys = []
     values = []
     question_marks = []
